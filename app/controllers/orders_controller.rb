280f6e5ff8e6524a5c7345a7beb49e2bde1e07cb
Lorenz Houze <lorenz.houze@gmail.com>
2025-06-05 17:20:47 +0200

    routes, ajout fontawsome dans head, cart order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,4 +4,16 @@ def index
   @orders = Order.all
 end
 
+def accepted
+  @order = Order.find(params[:id])
+  @order.update(status_store: "accepted")
+  redirect_to orders_path, notice: "Commande validée !"
+end
+
+def refused
+  @order = Order.find(params[:id])
+  @order.update(status_store: "refused")
+  redirect_to orders_path, notice: "Commande refusée !"
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,12 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  resources :orders
+  resources :orders do
+    member do
+    patch :accepted
+    patch :refused
+    end
+  end
   resources :stores
   resources :events
   resources :stores do
