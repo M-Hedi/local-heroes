19c2054e750e2fb6f7fdbf1d1ebc99e2294638ae
Chahnez Zurawicz <cvkroyal@hotmail.com>
2025-06-05 14:30:38 +0200

    retour de stash

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,6 +2,14 @@ class EventsController < ApplicationController
 
   def index
     @events = Event.all
+    # if params[:query].present?
+    #   sql_subquery = <<~SQL
+    #     events.title @@ :query
+    #     OR events.description @@ :query
+    #     OR stores.name @@ :query
+    #   SQL
+    #   @events = @events.joins(:store).where(sql_subquery, query: params[:query])
+    # end
   end
 
   def show
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,14 @@
 class Event < ApplicationRecord
+  include PgSearch::Model
   belongs_to :store
+
+pg_search_scope :global_search,
+  against: [ :title, :description ],
+  associated_against: {
+    store: [ :name]
+  },
+  using: {
+    tsearch: { prefix: true }
+  }
+
 end
