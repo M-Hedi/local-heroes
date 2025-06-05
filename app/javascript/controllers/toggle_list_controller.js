c11ec828689d337911abdc18a30399113e2172f7
Jonas Capelier <capelier.jonas@gmail.com>
2025-06-05 17:58:46 +0200

    WIP

--- app/javascript/controllers/toggle_list_controller.js
+++ app/javascript/controllers/toggle_list_controller.js
@@ -10,11 +10,13 @@ export default class extends Controller {
     this.storesBtnTarget.classList.add("toggle-active")
     this.productsBtnTarget.classList.remove("toggle-active")
     this.listsInnerTarget.style.transform = "translateX(0%)"
+    this.storesListTarget.scrollTop = 0;
   }
 
   displayProducts = () => {
     this.productsBtnTarget.classList.add("toggle-active")
     this.storesBtnTarget.classList.remove("toggle-active")
     this.listsInnerTarget.style.transform = "translateX(-50%)"
+    this.productsListTarget.scrollTop = 0;
   }
 }
