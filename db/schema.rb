5f6d32b40adf459f5b0873f0c358aad5a5b9e822
Lorenz Houze <lorenz.houze@gmail.com>
2025-06-09 12:59:59 +0200

    add phonenuber and address to user and store

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_06_06_134851) do
+ActiveRecord::Schema[7.1].define(version: 2025_06_09_095658) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -96,6 +96,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_06_06_134851) do
     t.string "phone_number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "address"
     t.index ["user_id"], name: "index_stores_on_user_id"
   end
 
@@ -109,6 +110,8 @@ ActiveRecord::Schema[7.1].define(version: 2025_06_06_134851) do
     t.datetime "updated_at", null: false
     t.string "first_name"
     t.string "last_name"
+    t.string "address"
+    t.string "phone_number"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
